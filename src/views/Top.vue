f53ddfb8134b671fa981a48e608af516383999df
HarutoAmetani <haruto.ametani@gmail.com>
2025-12-09 00:35:37 +0900

    feat:Vuetifyの不具合修正、トップ画面の実装

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,10 +1,42 @@
 <template>
-  <h1>トップページ</h1>
-  <RouterLink to="/register">新規登録</RouterLink><br/>
-  <RouterLink to="/login">ログイン</RouterLink><br/>
-  <RouterLink to="/forgot-password">パスワードリセット</RouterLink>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <v-list nav>
+        <v-list-item title="新規登録" link @click="toRegister"></v-list-item>
+        <v-list-item title="ログイン" link @click="toLogin"></v-list-item>
+        <v-list-item title="パスワードリセット" link @click="toForgotPassword"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>トップ</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router';
+import { useRouter } from 'vue-router';
+import { ref } from 'vue'
+
+const drawer = ref(false)
+
+const router = useRouter()
+
+function toRegister() {
+  router.push('/register')
+}
+
+function toLogin() {
+  router.push('/login')
+}
+
+function toForgotPassword() {
+  router.push('/forgot-password')
+}
 </script>
